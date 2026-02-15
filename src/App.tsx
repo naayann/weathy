4b6197828549e00cad86c41f17a2fd03793bc50f
Nayan Sarania <nayan.sarania.work@gmail.com>
2026-02-15 15:34:02 +0530

    install tanstack query

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Layout from "./components/layout"
 import { ThemeProvider } from "./context/theme-provider"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import Layout from "./components/layout"
 import Dashboard from "./pages/dashboard"
 import CityPage from "./pages/city-page"
 
+const queryClient = new QueryClient()
+
 function App() {
   return (
-    <BrowserRouter>
-      <ThemeProvider defaultTheme="dark">
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Dashboard/>}/>
-            <Route path="/city/:cityName" element={<CityPage/>}/>
-          </Routes>
-        </Layout>
-      </ThemeProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ThemeProvider defaultTheme="dark">
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/city/:cityName" element={<CityPage />} />
+            </Routes>
+          </Layout>
+        </ThemeProvider>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
 
