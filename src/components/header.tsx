3ff90a157507b8d2ea746b5f00e8172cc526a9b5
Nayan Sarania <nayan.sarania.work@gmail.com>
2026-02-15 15:09:03 +0530

    create dark mode functionality

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import { useTheme } from '@/context/theme-provider'
+import { Link } from 'react-router-dom'
+import { Sun, Moon } from 'lucide-react';
 
 const Header = () => {
+  const { theme, setTheme } = useTheme();
+  const isDark = theme === "dark";
+
   return (
-    <header>
-      Header
+    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur py-2">
+      <div className="container mx-auto flex h-16 items-center justify-between px-4">
+        <Link to="/">
+          <img
+            src={isDark ? "/logo-light.png" : "/logo-dark.jpg"}
+            alt="logo"
+            className={`h-12 ${isDark ? "" : "invert"}`}
+          />
+        </Link>
+
+        {/* theme toggle */}
+        <div 
+          onClick={() => setTheme(isDark ? "light" : "dark")}
+          className={`flex items-center cursor-pointer transition-transform duration-400 ${isDark?"rotate-90":"rotate-0"}`}
+          >
+          {isDark ? (
+            <Sun className='h-6 w-6 text-yellow-500 rotate-0 transition-all' />
+           ) : (
+            <Moon className='h-6 w-6 text-blue-500 rotate-0 transition-all' />
+           )}
+        </div>
+      </div>
     </header>
-  )
-}
+  );
+};
 
 export default Header
