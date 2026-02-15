d1dcea361a32bd9f5ca180049c22ef7a37364892
Nayan Sarania <nayan.sarania.work@gmail.com>
2026-02-16 02:00:57 +0530

    upload everything

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,16 @@ import Layout from "./components/layout"
 import Dashboard from "./pages/dashboard"
 import CityPage from "./pages/city-page"
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000,
+      gcTime: 10 * 60 * 1000,
+      retry: false,
+      refetchOnWindowFocus: false
+    }
+  }
+})
 
 function App() {
   return (
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,8 +1,61 @@
 import { Button } from '@/components/ui/button'
+import WeatherSkeleton from '@/components/ui/loading-skeleton'
 import { useGeolocation } from '@/hooks/use-geolocation'
 import { RefreshCw } from 'lucide-react'
+import { Alert, AlertTitle, AlertDescription } from '@/components/ui/alert'
+import { AlertCircleIcon, MapPin } from 'lucide-react'
 
 const Dashboard = () => {
+
+  const { 
+    coordinates, 
+    error: locationError, 
+    getLocation, 
+    isLoading: locationLoading
+   } = useGeolocation()
+
+  // const handleRefresh = () => {
+  //   getLocation()
+  //   if (coordinates) {
+  //     // reload weather data
+  //   }
+  // }
+
+  if(locationLoading) {
+    return <WeatherSkeleton />
+  }
+
+  if (locationError) {
+    return (
+    <Alert variant="destructive" className="max-w-md">
+      <AlertCircleIcon />
+      <AlertTitle>Location Error</AlertTitle>
+      <AlertDescription className='flex flex-col gap-4'>
+        <p>{locationError}</p>
+        <Button onClick={getLocation} variant={'outline'} className='w-fit'>
+          <MapPin className="mr-2 h-4 w-4" />
+          Enable Loaction
+        </Button>
+      </AlertDescription>
+    </Alert>
+  )
+  }
+
+  if (!coordinates) {
+    return (
+    <Alert variant="destructive">
+      <AlertCircleIcon />
+      <AlertTitle>Location Error</AlertTitle>
+      <AlertDescription className='flex flex-col gap-4'>
+        <p>Please enable location access to see your local weather.</p>
+        <Button onClick={getLocation} variant={'outline'} className='w-fit'>
+          <MapPin className="mr-2 h-4 w-4" />
+          Enable Loaction
+        </Button>
+      </AlertDescription>
+    </Alert>
+  )
+  }
 
   return (
     <div>
